01a994a356f7b83bf28a99e4893557d8149aff6c
f4js3il <shaluchandran@gmail.com>
2022-03-28 21:35:46 -0500

    updated changes

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
-module.exports={
-    branches: "master",
-    repositoryUrl: "https://github.com/f4js3il/my-app",
-    plugins: ["@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github"],
-}
+module.exports = {
+    branches: 'master',
+    repositoryUrl: 'https://github.com/f4js3il/my-app',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+    ],
+};
