ba62436c3af2b150df58e00d1cc6f14c24cc63cc
f4js3il <shaluchandran@gmail.com>
2022-03-28 21:24:42 -0500

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,7 @@
+module.exports={
+    branches: "master",
+    repositoryUrl: "https://github.com/f4js3il/my-app",
+    plugins: ["@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"],
+}
